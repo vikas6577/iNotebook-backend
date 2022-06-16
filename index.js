01f1450e89ca946bafeae884cc2b0de212932c92
vikas6577 <vs509411@gmail.com>
2022-06-16 10:43:06 +0530

    edit for frontend

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
 
 app.listen(port, () => {
-  console.log(`Example app listening on port  http://localhost:${port}`)
+  console.log(`iNotebook backend listening on port  http://localhost:${port}`)
 })
